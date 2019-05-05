b6862b71eec3d2e70ea75085a4520aebe0000940
许向飞 <xuxf@asiainfo.com>
2019-05-05 13:17:35 +0800

    第一次提交

--- src/com/feifei/Main.java
+++ src/com/feifei/Main.java
@@ -19,5 +19,7 @@ public class Main {
             e.printStackTrace();
         }
         System.out.println(":要获取的真实数据="+ data.getResult()+System.currentTimeMillis());
+
+        System.out.println("第一次提交代码");
     }
 }
